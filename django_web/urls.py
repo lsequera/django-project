9a757d529f1cedc576b02228e56bc1aad69434f7
Ling <lsequera@proton.me>
2023-01-23 23:00:30 -0500

    Fixing static files sources and images

--- django_web/urls.py
+++ django_web/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from portfolio import views
 
 urlpatterns = [
@@ -23,4 +26,4 @@ urlpatterns = [
     path('', include('portfolio.urls')),
     path('', include('accounts.urls')),
     path('', include('blog.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
